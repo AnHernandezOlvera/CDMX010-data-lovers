cda8579bdd0ffb8fde1d5965f8721a437630bff7
An <anahidisenadora@gmail.com>
2021-02-02 09:41:00 -0600

    función de filtrado

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
-import {filterByType, pokemons} from './data.js';
+import {filterByType, changeOrder} from './data.js';
 //menu responsive
 const menu = document.getElementById('menuIcon');
 menu.addEventListener('click', function() {
-var x = document.getElementById("topnav");
+let x = document.getElementById("topnav");
   if (x.className === "startmenu") {
     x.className += " responsive";
   } else {
@@ -105,4 +105,70 @@ selector.addEventListener('change', function(event) {
     // pokemonsFiltered generar HTML como cards u otro y luego pasarlo a el contenedor
     //document.getElementById('container').innerHTML = 
 });
+
+// aplicación de ordemiento en evento
+const orderDefine = document.getElementById('orderSelect');
+orderDefine.addEventListener('change', function (event) {
+    const container = document.getElementById("pokemonlist");
+    const value = event.target.value;
+    if (value ==='A-Z'){
+        const ascendente = changeOrder(1, -1);
+        const listaAscendente = ascendente.map(function (pokemon) {
+            return`
+            <div class="card"> 
+            <img src="${pokemon.img}">
+            <p class="name">${pokemon.num} ${pokemon.name}</p>
+            <p class="type">Tipo: ${pokemon.type.join(', ')}</p>
+            <p class="physical">Peso: ${pokemon.size.weight} | Altura: ${pokemon.size.height}</p>
+            <div class="footer-card">
+            <span class="generation">${pokemon.generation.name}</span>
+            </div>
+            </div>
+    `;  
+    }); container.innerHTML=listaAscendente.join('');
+        
+    }
+    if (value ==='Z-A'){
+        const descendente = changeOrder(-1, 1);
+        const listaDescendente = descendente.map(function (pokemon) {
+            return`
+            <div class="card"> 
+            <img src="${pokemon.img}">
+            <p class="name">${pokemon.num} ${pokemon.name}</p>
+            <p class="type">Tipo: ${pokemon.type.join(', ')}</p>
+            <p class="physical">Peso: ${pokemon.size.weight} | Altura: ${pokemon.size.height}</p>
+            <div class="footer-card">
+            <span class="generation">${pokemon.generation.name}</span>
+            </div>
+            </div>
+    `;  
+    }); container.innerHTML=listaDescendente.join('');
+        
+    }
+})/* 
+document.getElementById('orderSelect').addEventListener('change', function (event) {
+    let value = event.target.value;
+    if (value === "AZ") {
+        const container= document.getElementById('pokemonlist');
+        const ascendingList = orderlist.map(function (pokemon) {
+            return`
+            <div class="card"> 
+        <img src="${pokemon.img}">
+      <p class="name">${pokemon.num} ${pokemon.name}</p>
+      <p class="type">Tipo: ${pokemon.type.join(', ')}</p>
+      <p class="physical">Peso: ${pokemon.size.weight} | Altura: ${pokemon.size.height}</p>
+      <div class="footer-card">
+      <span class="generation">${pokemon.generation.name}</span>
+      </div>
+      </div>
+    `;  
+    }); container.innerHTML=ascendingList.join('');
+    
+  } else if (event.target.value==='ZA'){
+
+  }
+    else {
+    console.log('error');
+  }
+}) */
 
